6dd6e861fc77d96a4c56f1bca896d0d213685607
Korliakov Denys <korl.den66@gmail.com>
2023-04-08 22:59:16 +0300

    catCarouselFinally

--- task/script/script.js
+++ task/script/script.js
@@ -20,12 +20,34 @@ class CarouselController {
         const catArray = JSON.parse(data);
 
         for (const index in catArray) {
+            const catContainer = document.createElement("div")
+            catContainer.classList.add("catContainer")
+
             const catImage = document.createElement("img")
             catImage.src = catArray[index].imageUrl;
             catImage.onerror = ()=>{
                 catImage.src = "images/cat.webp"
             }
-            this.#images.appendChild(catImage);
+            catContainer.appendChild(catImage);
+
+            const catDescription = document.createElement("div");
+            catDescription.classList.add("description")
+
+            const createdAt = document.createElement("span");
+            const author = document.createElement("span");
+            const authorLink = document.createElement("span");
+
+            createdAt.textContent = "Created: " + catArray[index].createdAt;
+            author.textContent = "Author: " + catArray[index].author;
+            authorLink.textContent = "Author Link: " + catArray[index].authorLink;
+
+            catDescription.appendChild(createdAt)
+            catDescription.appendChild(author)
+            catDescription.appendChild(authorLink)
+
+            catContainer.appendChild(catDescription)
+
+            this.#images.appendChild(catContainer);
         }
         console.log("initialized")
     }
